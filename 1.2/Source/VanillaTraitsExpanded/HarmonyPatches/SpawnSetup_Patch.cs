b140c7774cba0fc21a9481d98ad781e81c324023
Taranchuk <nfhfyxer@gmail.com>
2021-03-11 08:17:38 +0200

    update

--- 1.2/Source/VanillaTraitsExpanded/HarmonyPatches/SpawnSetup_Patch.cs
+++ 1.2/Source/VanillaTraitsExpanded/HarmonyPatches/SpawnSetup_Patch.cs
@@ -31,83 +31,90 @@ namespace VanillaTraitsExpanded
         }
         public static void AddPawn(Pawn __instance)
         {
-            if (__instance.HasTrait(VTEDefOf.VTE_Coward))
+            try
             {
-                TraitUtils.TraitsManager.cowards.Add(__instance);
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_BigBoned))
-            {
-                TraitUtils.TraitsManager.bigBoned.Add(__instance);
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_Submissive))
-            {
-                if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_SlowWorkSpeed) == null)
+                if (__instance.HasTrait(VTEDefOf.VTE_Coward))
                 {
-                    var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_SlowWorkSpeed, __instance);
-                    __instance.health.AddHediff(hediff);
+                    TraitUtils.TraitsManager.cowards.Add(__instance);
                 }
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_Snob))
-            {
-                TraitUtils.TraitsManager.snobs.Add(__instance);
-            }
+                if (__instance.HasTrait(VTEDefOf.VTE_BigBoned))
+                {
+                    TraitUtils.TraitsManager.bigBoned.Add(__instance);
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_Submissive))
+                {
+                    if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_SlowWorkSpeed) == null)
+                    {
+                        var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_SlowWorkSpeed, __instance);
+                        __instance.health.AddHediff(hediff);
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_Snob))
+                {
+                    TraitUtils.TraitsManager.snobs.Add(__instance);
+                }
 
-            if (__instance.HasTrait(VTEDefOf.VTE_MadSurgeon))
-            {
-                if (TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick == null)
-                    TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick = new Dictionary<Pawn, int>();
-                if (!TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick.ContainsKey(__instance))
+                if (__instance.HasTrait(VTEDefOf.VTE_MadSurgeon))
                 {
-                    TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick[__instance] = GenTicks.TicksAbs;
+                    if (TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick == null)
+                        TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick = new Dictionary<Pawn, int>();
+                    if (!TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick.ContainsKey(__instance))
+                    {
+                        TraitUtils.TraitsManager.madSurgeonsWithLastHarvestedTick[__instance] = GenTicks.TicksAbs;
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_Stoner))
+                {
+                    if (!__instance.health.hediffSet.HasHediff(VTEDefOf.SmokeleafAddiction))
+                    {
+                        var hediff = HediffMaker.MakeHediff(VTEDefOf.SmokeleafAddiction, __instance);
+                        __instance.health.AddHediff(hediff);
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_Lush))
+                {
+                    if (!__instance.health.hediffSet.HasHediff(VTEDefOf.AlcoholAddiction))
+                    {
+                        var hediff = HediffMaker.MakeHediff(VTEDefOf.AlcoholAddiction, __instance);
+                        __instance.health.AddHediff(hediff);
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_Wanderlust))
+                {
+                    if (TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick == null) TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick = new Dictionary<Pawn, int>();
+                    if (!TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick.ContainsKey(__instance))
+                    {
+                        TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick[__instance] = GenTicks.TicksAbs;
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_Insomniac))
+                {
+                    if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_RestSlowFallFactor) == null)
+                    {
+                        var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_RestSlowFallFactor, __instance);
+                        __instance.health.AddHediff(hediff);
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_ThickSkinned))
+                {
+                    if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_SlowerBleedingRate) == null)
+                    {
+                        var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_SlowerBleedingRate, __instance);
+                        __instance.health.AddHediff(hediff);
+                    }
+                }
+                if (__instance.HasTrait(VTEDefOf.VTE_ThinSkinned))
+                {
+                    if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_HigherBleedingRate) == null)
+                    {
+                        var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_HigherBleedingRate, __instance);
+                        __instance.health.AddHediff(hediff);
+                    }
                 }
             }
-            if (__instance.HasTrait(VTEDefOf.VTE_Stoner))
+            catch (Exception ex)
             {
-                if (!__instance.health.hediffSet.HasHediff(VTEDefOf.SmokeleafAddiction))
-                {
-                    var hediff = HediffMaker.MakeHediff(VTEDefOf.SmokeleafAddiction, __instance);
-                    __instance.health.AddHediff(hediff);
-                }
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_Lush))
-            {
-                if (!__instance.health.hediffSet.HasHediff(VTEDefOf.AlcoholAddiction))
-                {
-                    var hediff = HediffMaker.MakeHediff(VTEDefOf.AlcoholAddiction, __instance);
-                    __instance.health.AddHediff(hediff);
-                }
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_Wanderlust))
-            {
-                if (TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick == null) TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick = new Dictionary<Pawn, int>();
-                if (!TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick.ContainsKey(__instance))
-                {
-                    TraitUtils.TraitsManager.wanderLustersWithLastMapExitedTick[__instance] = GenTicks.TicksAbs;
-                }
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_Insomniac))
-            {
-                if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_RestSlowFallFactor) == null)
-                {
-                    var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_RestSlowFallFactor, __instance);
-                    __instance.health.AddHediff(hediff);
-                }
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_ThickSkinned))
-            {
-                if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_SlowerBleedingRate) == null)
-                {
-                    var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_SlowerBleedingRate, __instance);
-                    __instance.health.AddHediff(hediff);
-                }
-            }
-            if (__instance.HasTrait(VTEDefOf.VTE_ThinSkinned))
-            {
-                if (__instance.health.hediffSet.GetFirstHediffOfDef(VTEDefOf.VTE_HigherBleedingRate) == null)
-                {
-                    var hediff = HediffMaker.MakeHediff(VTEDefOf.VTE_HigherBleedingRate, __instance);
-                    __instance.health.AddHediff(hediff);
-                }
+                Log.Error($"Exception checking traits in {__instance}: {ex}");
             }
         }
     }
