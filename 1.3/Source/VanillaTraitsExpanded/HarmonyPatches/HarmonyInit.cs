ed67578acb32f661de89c4e496e283e9623956bf
Taranchuk <nfhfyxer@gmail.com>
2021-08-25 01:03:28 +0300

    disabled logging

--- 1.3/Source/VanillaTraitsExpanded/HarmonyPatches/HarmonyInit.cs
+++ 1.3/Source/VanillaTraitsExpanded/HarmonyPatches/HarmonyInit.cs
@@ -58,33 +58,33 @@ namespace VanillaTraitsExpanded
 	//	}
 	//}
 
-	[HarmonyPatch(typeof(MemoryThoughtHandler), "RemoveMemory")]
-	public static class RemoveMemory_Patch
-	{
-		public static void Postfix(MemoryThoughtHandler __instance, Thought_Memory th)
-		{
-			Log.Message("Removing " + th.def + " from " + __instance.pawn);
-		}
-	}
-
-	[HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[] {
-	   typeof(Thought_Memory),
-	   typeof(Pawn)
-	})]
-	public static class TryGainMemory_Patch2
-	{
-		private static void Postfix(MemoryThoughtHandler __instance, Thought_Memory newThought, Pawn otherPawn)
-		{
-			Log.Message("Adding " + newThought.def + " to " + __instance.pawn);
-		}
-	}
-
-	[HarmonyPatch(typeof(IndividualThoughtToAdd), "Add")]
-	public static class IndividualThoughtToAdd_Patch
-	{
-		public static void Postfix(IndividualThoughtToAdd __instance)
-		{
-			Log.Message("2 Adding " + __instance.thought.def + " to " + __instance.addTo);
-		}
-	}
+	//[HarmonyPatch(typeof(MemoryThoughtHandler), "RemoveMemory")]
+	//public static class RemoveMemory_Patch
+	//{
+	//	public static void Postfix(MemoryThoughtHandler __instance, Thought_Memory th)
+	//	{
+	//		Log.Message("Removing " + th.def + " from " + __instance.pawn);
+	//	}
+	//}
+	//
+	//[HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[] {
+	//   typeof(Thought_Memory),
+	//   typeof(Pawn)
+	//})]
+	//public static class TryGainMemory_Patch2
+	//{
+	//	private static void Postfix(MemoryThoughtHandler __instance, Thought_Memory newThought, Pawn otherPawn)
+	//	{
+	//		Log.Message("Adding " + newThought.def + " to " + __instance.pawn);
+	//	}
+	//}
+	//
+	//[HarmonyPatch(typeof(IndividualThoughtToAdd), "Add")]
+	//public static class IndividualThoughtToAdd_Patch
+	//{
+	//	public static void Postfix(IndividualThoughtToAdd __instance)
+	//	{
+	//		Log.Message("2 Adding " + __instance.thought.def + " to " + __instance.addTo);
+	//	}
+	//}
 }
